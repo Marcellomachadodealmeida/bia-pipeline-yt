e84727d1a276827e4614d70e6ecb56b6b867e918
Marcello Almeida <marcelloalmeidati@hotmail.com>
2024-11-19 17:39:41 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
                   onToggle={toggleReminder}
                 />
               ) : (
-                "Nenhuma tarefa cadastrada ate agora"
+                "Pronto para receber novas tarefas"
               )}
             </>
           )}
